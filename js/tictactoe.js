eb3592e83ff41fde4479f5d609aec7e4343e260d
Kole Morgan <kmorgan0057@gmail.com>
2020-12-29 16:19:37 -0600

    Tic Tac Toe Assets

---
+++ js/tictactoe.js
@@ -0,0 +1,18 @@
+"use strict"
+
+
+
+
+
+const tiles = [
+    {id: 1, xState: null, oState:null},
+    {id: 2, xState: null, oState:null},
+    {id: 3, xState: null, oState:null},
+    {id: 4, xState: null, oState:null},
+    {id: 5, xState: null, oState:null},
+    {id: 6, xState: null, oState:null},
+    {id: 7, xState: null, oState:null},
+    {id: 8, xState: null, oState:null},
+    {id: 9, xState: null, oState:null},
+    {id: 10, xState: null, oState:null},
+];
