d905471a4b79af58d10ec9ec5c8d4a8b5f1002d8
Kole Morgan <kmorgan0057@gmail.com>
2020-12-30 15:09:39 -0600

    Tictactoe Functionality

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,7 +1,49 @@
 "use strict"
+// class Tictactoe{
+//     constructor(display){
+//         this.display = display;
+//         this.reset();
+//     }
+//
+//     updateXDisplay(img){
+//         this.display.innerHTML= img
+//
+//     }
+// }
 
 
 
+
+const display = document.querySelectorAll('[data-tile]');
+const reset = document.querySelector('[data-reset]');
+const ximage = "img/X.png";
+const oimage = "img/O.png";
+
+// const tictactoe = new Tictactoe(display);
+
+function updateXDisplay(tile){
+    while(state) {
+        const img = document.createElement("img");
+        img.src = ximage;
+        tile.appendChild(img);
+    }
+
+}
+
+display.forEach(tile=>{
+    tile.addEventListener('click',()=>{
+         updateXDisplay(tile)
+        // tictactoe.checkWinner()
+        // tictactoe.computerGoes()
+        // tictactoe.updateDisplay()
+        // tictactoe.checkWinner()
+    })
+})
+
+reset.addEventListener('click',()=>{
+    tictactoe.clear()
+    tictactoe.updateDisplay()
+})
 
 
 const tiles = [
@@ -16,3 +58,5 @@ const tiles = [
     {id: 9, xState: null, oState:null},
     {id: 10, xState: null, oState:null},
 ];
+
+document.getElementById('x1').style.visibility='none';
