22bec45bda6fde8ffe2b03fffe980867667d9807
Kole Morgan <kmorgan0057@gmail.com>
2020-12-30 17:00:15 -0600

    tictactoe functionality

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,15 +1,13 @@
 "use strict"
-// class Tictactoe{
-//     constructor(display){
-//         this.display = display;
-//         this.reset();
-//     }
-//
-//     updateXDisplay(img){
-//         this.display.innerHTML= img
-//
-//     }
-// }
+class Tictactoe{
+    constructor(display){
+        this.display = display;
+        this.reset();
+    }
+
+
+
+}
 
 
 
@@ -18,45 +16,74 @@ const display = document.querySelectorAll('[data-tile]');
 const reset = document.querySelector('[data-reset]');
 const ximage = "img/X.png";
 const oimage = "img/O.png";
+const playerArray = [];
+const compArray = [];
+const winningArray = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 // const tictactoe = new Tictactoe(display);
 
 function updateXDisplay(tile){
-    while(state) {
         const img = document.createElement("img");
         img.src = ximage;
         tile.appendChild(img);
-    }
+        const num = tile.getAttribute("id")
+        playerArray.push(parseInt(num))
+        console.log(playerArray)
+        playerArray.sort();
+
 
 }
+function isTrue(arr, arr2){
+    return arr.some(i=>arr2.includes(i))
+}
+function checkWinner(arr1, arr2) {
+    let a =
+    for(let i=0;i<arr2.length; i++){
+        if(isTrue(arr1,arr2[i])) {
+            console.log(true)
+        }else {
+            console.log(false)
+        }
+        }
+}
 
+
+function updateODisplay(tile){
+    const img = document.createElement("img");
+    img.src = oimage;
+    tile.appendChild(img);
+
+}
 display.forEach(tile=>{
     tile.addEventListener('click',()=>{
          updateXDisplay(tile)
-        // tictactoe.checkWinner()
         // tictactoe.computerGoes()
-        // tictactoe.updateDisplay()
+        // tictactoe.checkWinner()
+    })
+})
+display.forEach(tile=>{
+    tile.addEventListener('click',()=>{
+        checkWinner(playerArray,winningArray);
+        // tictactoe.computerGoes()
         // tictactoe.checkWinner()
     })
 })
 
-reset.addEventListener('click',()=>{
-    tictactoe.clear()
-    tictactoe.updateDisplay()
-})
+// reset.addEventListener('click',()=>{
+//     tictactoe.clear()
+//     tictactoe.updateDisplay()
+// })
 
 
-const tiles = [
-    {id: 1, xState: null, oState:null},
-    {id: 2, xState: null, oState:null},
-    {id: 3, xState: null, oState:null},
-    {id: 4, xState: null, oState:null},
-    {id: 5, xState: null, oState:null},
-    {id: 6, xState: null, oState:null},
-    {id: 7, xState: null, oState:null},
-    {id: 8, xState: null, oState:null},
-    {id: 9, xState: null, oState:null},
-    {id: 10, xState: null, oState:null},
-];
-
-document.getElementById('x1').style.visibility='none';
+// const tiles = [
+//     {id: 1, xState: null, oState:null},
+//     {id: 2, xState: null, oState:null},
+//     {id: 3, xState: null, oState:null},
+//     {id: 4, xState: null, oState:null},
+//     {id: 5, xState: null, oState:null},
+//     {id: 6, xState: null, oState:null},
+//     {id: 7, xState: null, oState:null},
+//     {id: 8, xState: null, oState:null},
+//     {id: 9, xState: null, oState:null},
+//     {id: 10, xState: null, oState:null},
+// ];
